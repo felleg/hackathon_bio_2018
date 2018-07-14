f78db821f31dc47ade2859f84147aeffab79a006
Felix Leger <felixleger@hotmail.com>
2018-07-13 21:45:31 -0400

    little modifs

--- analysis.py
+++ analysis.py
@@ -6,14 +6,17 @@ import tensorflow as tf
 truth = pd.read_csv('ISIC2018_Task3_Training_GroundTruth/ISIC2018_Task3_Training_GroundTruth.csv')
 
 # Sample 1% of the data for development because my computer is bad
-truth = truth.sample(frac=0.01)
+truth = truth.sample(frac=0.1)
 
 bgr = []
 
 # Loop over melanomas
 for index in range(len(truth)):
+
+    # Print current progress
     if index%100 == 0:
         print(str(index) + ' / ' + str(len(truth)))
+
     image = truth.iloc[index].image
     bgr += [cv2.imread('ISIC2018_Task3_Training_Input/'+image+'.jpg')]
 
